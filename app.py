b53db761c8c30a1eec5a0bfad51878ac1f017718
NapoleonPerez <87319901+napo178@users.noreply.github.com>
2022-05-24 13:35:16 -0600

    ff
    f

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 st.title('Real Time order prediction')
 
 from PIL import Image
-image = Image.open('crmb.png')
+image = Image.open('/Users/napoleonperez/Desktop/app_order/crmb.png')
 st.image(image, caption='crmb')
 st.write('This app predicts the order time  based on the location amd the status based on the historical data')
 
@@ -32,7 +32,7 @@ customer_id= st.number_input('customer_id', 1.0)
 
 
 if st.button("Predict"):
-    pickle_in = open('app_order/model.pkl', 'rb')
+    pickle_in = open('/Users/napoleonperez/Desktop/app_order/model.pkl', 'rb')
     model = pickle.load(pickle_in)
     predict=model.predict([[order_id,location_id,total,status_class,customer_id]])
   
