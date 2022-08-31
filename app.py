be55b2ad8771101a72e1bb8b4a064ad57b4404f3
Napoleón Pérez <87319901+napo178@users.noreply.github.com>
2022-08-31 03:12:42 -0600

    df
    d

--- app.py
+++ app.py
@@ -3,51 +3,47 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
-from sklearn import linear_model
 
-st.title('Real Time order prediction')
+st.title('App to predict multiple intelligence')
+
+
+
+
 from PIL import Image
 image = Image.open('int.png')
 st.image(image, caption='Multiple Intelligence')
 
-st.title('The app uses the real time inputs to predict the final score in the Gardners Multiple Intelligence test')
-
-
-
+# features for prediction 
+# X = sales_join[['a_value', 'text_categorye', 'id','id','question_id']]
+#y = sales_join['items_total']
 
 # Add a heading for input features
 st.subheader('Enter  Features for Predictions')
 
  # Rwquest for input fatures, but replod with some default values
 a_order= st.number_input('a_order', 1.0)
+st.write(' The a_value is :', a_order)
 
-st.write(' The a_order is:', a_order)
+
 
 a_value= st.number_input('a_value', 1.0)
+st.write(' The a_value is :', a_value)
 
-st.write('The a_value is:', a_value)
 
 question_id= st.number_input('question_id', 1.0)
-
-st.write('The question_id is:',question_id)
-
-
-text_category= st.selectbox('text_category', ['Mostly Disagree=1', 'Slightly Disagree=2', 'Slightly Agree=3', 'Mostly Agree=4'])
+st.write(' The question_id :', question_id)
 
 
-text_category= st.number_input('status_class', 1.0)
 
-st.write('The status_class is', text_category)
+text_category= st.number_input('text_category', 1.0)
+
+st.write(' The text_categorye is :', text_category)
 
 
 id= st.number_input('id', 1.0)
-
-st.write('the id is', id)
-
+st.write('The id is', id)
 
 
-
-st.write('(The model made a prediction for multiple intelligence for each people in the survey')
 
 
 
@@ -55,15 +51,14 @@ st.write('(The model made a prediction for multiple intelligence for each people
 
 # Load  the model from disk
 
-
 if st.button("Predict"):
     pickle_in = open('model.pkl', 'rb')
     model = pickle.load(pickle_in)
-    predict=model.predict(['a_order','a_value','question_id','text_category','id'])
+    predict=model.predict([[a_order,a_value,question_id,text_category,id]])
   
 
     st.text(f"""
-     The predicted intelligence multiple  is :  {predict[0]} 
+     The number of items in the a_value is :  {predict[0]} 
     """)    # Get the input features
     # run predictions
 
